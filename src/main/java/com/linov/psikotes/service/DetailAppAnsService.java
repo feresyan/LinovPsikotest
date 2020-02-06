db688a4db672bf7a8e0dcd7c57062189eee06569
feresyan <feresyan@gmail.com>
2020-02-06 11:45:44 +0700

    update report dao

--- src/main/java/com/linov/psikotes/service/DetailAppAnsService.java
+++ src/main/java/com/linov/psikotes/service/DetailAppAnsService.java
@@ -176,7 +176,8 @@ public class DetailAppAnsService {
 			if(pq.getQuestion().getQuestionType().getQuestionTypeTitle().equalsIgnoreCase("Pilihan Ganda Teks") || pq.getQuestion().getQuestionType().getQuestionTypeTitle().equalsIgnoreCase("Pilihan Ganda Gambar")) {
 				if(pq.getQuestion().getCorrectAnswer().getAnswer1() != null && pq.getQuestion().getCorrectAnswer().getAnswer2() != null) {
 										
-					if(pq.getQuestion().getCorrectAnswer().getAnswer1().equalsIgnoreCase(data.getAppAnswer().getAnswer1()) && pq.getQuestion().getCorrectAnswer().getAnswer2().equalsIgnoreCase(data.getAppAnswer().getAnswer2())) {
+					if( ( pq.getQuestion().getCorrectAnswer().getAnswer1().equalsIgnoreCase(data.getAppAnswer().getAnswer1()) || pq.getQuestion().getCorrectAnswer().getAnswer1().equalsIgnoreCase(data.getAppAnswer().getAnswer2())) 
+							&& (pq.getQuestion().getCorrectAnswer().getAnswer2().equalsIgnoreCase(data.getAppAnswer().getAnswer2()) || pq.getQuestion().getCorrectAnswer().getAnswer2().equalsIgnoreCase(data.getAppAnswer().getAnswer1())) ) {
 						tempPoints+=10;
 					}
 				}
