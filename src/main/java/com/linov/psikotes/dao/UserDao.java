dab58351527d311afe860daae771af9d1388ac7b
feresyan <feresyan@gmail.com>
2020-02-06 09:53:33 +0700

    update userDao loadByUsername

--- src/main/java/com/linov/psikotes/dao/UserDao.java
+++ src/main/java/com/linov/psikotes/dao/UserDao.java
@@ -101,9 +101,8 @@ public class UserDao extends CommonDao {
 	@Transactional
 	public User loadUserByUsername(String username) {
 		List<User> list = super.entityManager
-				.createQuery("from User where username = :field1 and role_id = :field2")
+				.createQuery("from User where username = :field1")
 				.setParameter("field1", username)
-				.setParameter("field2", "role2")
 				.getResultList();
 		if(list.size()==0)
 			return new User();
